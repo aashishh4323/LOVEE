2df90ce7e633314fc0486e964a7b7a48d6fe25da
Ashish kumar <166837904+aashishh4323@users.noreply.github.com>
2024-10-29 08:23:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.querySelectorAll('.yes-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        alert("Hyeeeeeeeee yrrrrrrrr kitni Acchii ho  ufff!!  teri  hi hawaleee ho gyaa abb mai tohhhh........");
+    });
+});
